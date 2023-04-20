7f75de8624e51c18d24eef9ba7e33b8086a34770
LHI0915 <gowns1998@gmail.com>
2023-04-21 03:20:37 +0900

    Feat:Add product-router

---
+++ src/routers/product-router.js
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+import { Product } from '../db/models/product-model';
+
+const productRouter = Router();
+
+productRouter.get('/products/:productId', async(req, res, next) => {
+    try{
+        const productId = req.params.productId;
+        const productInfo = await Product.findById({productId});
+
+        res.status(200).json(productInfo);
+    }catch(err){
+        next(err);
+    }
+    
+});
+
+// 관리자 기능
+
+//카테고리 추가
+
+//카테고리 수정
+
+//카테고리 삭제
+
+export default productRouter;
