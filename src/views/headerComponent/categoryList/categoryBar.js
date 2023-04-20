7d642dec47de5cc2d2ea88f7156fb30f085e833a
seob2 <fkdldjs120@gmail.com>
2023-04-20 19:28:05 +0900

    Feat: Add Header CategoryLayout

--- src/views/headerComponent/categoryList/categoryBar.js
+++ src/views/headerComponent/categoryList/categoryBar.js
@@ -1,15 +1,51 @@
-const headerContainer = document.querySelector("body");
+import { categories } from "./categories.js";
+const categoryBtn = document.getElementById("category-btn");
 
-async function categoryBar() {
-    return `
-    <div class="category-container">
-        <div class="category-item"></div>
-    </div>`
+export async function addCategoryBar(){
+    await makeCategoryBar();
+
+    const categoryContainer = document.querySelector('.category-container');
+    const closeBtn = document.getElementById("category-close-btn");
+    categoryBtn.addEventListener("click", ()=>{
+        const className = 'category-container-clicked';
+        categoryContainer.classList.toggle(className);
+    });
+    closeBtn.addEventListener("click", ()=>{
+        const className = 'category-container-clicked';
+        categoryContainer.classList.toggle(className);
+    });
+
 }
 
-headerContainer.insertAdjacentHTML( 'beforeend', `
-<div class="category-container">
-    <div class="category-item"></div>
-</div>`);
+async function makeCategoryBar() {
+    const headerContainer = document.querySelector("header");
+    const container = document.createElement('div');
+    container.classList.add('category-container');
+    container.insertAdjacentHTML(`beforeend`,`
+        <div class="category-title">
+            <span>Category</span>
+            <button id="category-close-btn">
+            <img src="../../mainPage/icon/free-icon-close-window-6583137.png" alt="close icon"/>
+            </button>
+        </div>
+    `)
+
+    categories.map(({type, iconSrc})=>{
+        container.insertAdjacentHTML(`beforeend`,
+        `<div class="category-item">
+              <a ref="/">
+              <img src="${iconSrc}" alt="${type} icon"/>
+              <span>${type}</span>
+              <img src="../../mainPage/icon/free-icon-arrow-right-6423875.png" alt="arrow icon"/> 
+              </a>
+          </div>
+        `);  
+      })
+      
+    headerContainer.appendChild(container);
+}
+
+
+
 
 
--- src/views/mainPage/js/main.js
+++ src/views/mainPage/js/main.js
@@ -1,5 +1,7 @@
 import { getMainCategoryData } from './category.js';
+import { addCategoryBar } from '../../headerComponent/categoryList/categoryBar.js';
 // 헤더가 보이게 임포트
 // import header from '../header/'
 
+addCategoryBar();
 getMainCategoryData();
