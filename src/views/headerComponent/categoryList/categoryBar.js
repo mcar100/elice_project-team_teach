c430bac8433227cce237d3a2688a69140977c025
David9191 <jisung9105@gmail.com>
2023-04-22 21:25:20 +0900

    Refactor: refactor with code review

--- src/views/headerComponent/categoryList/categoryBar.js
+++ src/views/headerComponent/categoryList/categoryBar.js
@@ -1,59 +1,65 @@
-import { categories } from "./categories.js";
+import { categories } from './categories.js';
 
-async function addCategoryBar(){
-    await makeCategoryBar();
+async function addCategoryBar() {
+  await makeCategoryBar();
 
-    const categoryBtn = document.getElementById("category-btn");
-    const categoryContainer = document.querySelector('.category-container');
-    const closeBtn = document.getElementById("category-close-btn");
+  const categoryBtn = document.getElementById('category-btn');
+  const categoryContainer = document.querySelector('.category-container');
+  const closeBtn = document.getElementById('category-close-btn');
 
-    categoryBtn.addEventListener("click", ()=>{
-        const className = 'category-container-clicked';
-        categoryContainer.classList.toggle(className);
-    });
+  categoryBtn.addEventListener('click', () => {
+    const className = 'category-container-clicked';
+    categoryContainer.classList.toggle(className);
+  });
 
-    closeBtn.addEventListener("click", ()=>{
-        const className = 'category-container-clicked';
-        categoryContainer.classList.toggle(className);
-    });
-
+  closeBtn.addEventListener('click', () => {
+    const className = 'category-container-clicked';
+    categoryContainer.classList.toggle(className);
+  });
 }
 
 function makeCategoryBar() {
-    const headerContainer = document.getElementById("header-container");
-    const headerLogo = document.querySelector(".header-logo");
-    const container = document.createElement('div');
-    container.classList.add('category-container');
+  const headerContainer = document.getElementById('header-container');
+  const headerLogo = document.querySelector('.header-logo');
+  const container = document.createElement('div');
+  container.classList.add('category-container');
 
-    headerLogo.insertAdjacentHTML('beforebegin',`
+  headerLogo.insertAdjacentHTML(
+    'beforebegin',
+    `
     <button id="category-btn">
         <img src="../../mainPage/icon/menu.png" alt="category icon" />
     </button>
-    `);
-    
-    container.insertAdjacentHTML(`beforeend`,`
+    `
+  );
+
+  container.insertAdjacentHTML(
+    `beforeend`,
+    `
         <div class="category-title">
             <span>Category</span>
             <button id="category-close-btn">
             <img src="../../mainPage/icon/free-icon-close-window-6583137.png" alt="close icon"/>
             </button>
         </div>
-    `)
+    `
+  );
 
-    categories.map(({type, iconSrc})=>{
-        container.insertAdjacentHTML(`beforeend`,
-        `<div class="category-item">
+  categories.map(({ type, iconSrc }) => {
+    container.insertAdjacentHTML(
+      `beforeend`,
+      `<div class="category-item">
               <a ref="/">
               <img src="${iconSrc}" alt="${type} icon"/>
               <span>${type}</span>
               <img src="../../mainPage/icon/free-icon-arrow-right-6423875.png" alt="arrow icon"/> 
               </a>
           </div>
-        `);  
-      })
-      
-    headerContainer.appendChild(container);
+        `
+    );
+  });
+
+  headerContainer.appendChild(container);
 }
 
 export { addCategoryBar };
-
--- src/views/mainPage/js/banner.js
+++ src/views/mainPage/js/banner.js
@@ -20,10 +20,8 @@ const wideBillboardColor = [
 
 const mainBillboard = document.querySelector('.main-billboard');
 const mainWideBillboard = document.querySelector('.main-wide-billboard');
-const mainNarrowBillboard = document.querySelector(
-  '.main-narrow-billboard > img'
-);
-let imagesIdx = 1;
+const mainNarrowBillboard = document.querySelector('.main-narrow-image');
+let imageIndex = 1;
 
 /**
  * 1. 맨 뒤에 배경색이 바뀌고
@@ -31,17 +29,16 @@ let imagesIdx = 1;
  * 3. 옆에 배경색이 바뀌어야 된다.
  * 4. 이미지 배경은 단일색으로 되어야 좋다. 11번가 처럼 전부 똑같은 배경색에 사람 or 상품만 다른 색으로 가져가야 자연스럽게 바뀐다!! 정말 중요!!
  */
-const changeBannerImages = async () => {
-  mainBillboard.style.backgroundColor = wideBillboardColor[imagesIdx];
+const changeBannerImages = () => {
+  mainBillboard.style.backgroundColor = wideBillboardColor[imageIndex];
   mainNarrowBillboard.style.opacity = 0;
-  mainWideBillboard.style.backgroundColor = wideBillboardColor[imagesIdx];
+  mainWideBillboard.style.backgroundColor = wideBillboardColor[imageIndex];
 
   setTimeout(() => {
     mainNarrowBillboard.style.opacity = 1;
-    mainWideBillboard.style.backgroundColor = wideBillboardColor[imagesIdx];
-    mainNarrowBillboard.setAttribute('src', images[imagesIdx]);
-    imagesIdx++;
-    if (imagesIdx === images.length) imagesIdx = 0;
+    mainWideBillboard.style.backgroundColor = wideBillboardColor[imageIndex];
+    mainNarrowBillboard.setAttribute('src', images[imageIndex]);
+    imageIndex = (imageIndex + 1) % images.length;
   }, 300);
 };
 
