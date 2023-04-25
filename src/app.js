abaf13cef13236eec98a5ab757392843dde3a7a9
LHI0915 <gowns1998@gmail.com>
2023-04-25 21:15:45 +0900

    Fix:Modify gitignore file

--- src/app.js
+++ src/app.js
@@ -6,8 +6,6 @@ import {
   productRouter,
   orderRouter,
 } from './routers/index.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 const app = express();
 
@@ -18,11 +16,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// console.log(app.use(express.static('views')));
-
-// app.get('/', (req, res) => {
-//   res.sendFile('../views/mainPage/html/mainPage.html');
-// });
 //api 라우팅
 
 app.use(viewRouter);
@@ -30,21 +23,5 @@ app.use(viewRouter);
 app.use('/categories', categoryRouter); // /api/catgories
 app.use('/products', productRouter);
 app.use('/orders', orderRouter);
-
-/*
-function serveStatic(resource) {
-  const __dirname = path.dirname(fileURLToPath(import.meta.url));
-  const resourcePath = path.join(__dirname, `../views/${resource}`);
-  const option = { index: `${resource}.html` };
-
-  console.log('__dirname', __dirname);
-  console.log('resourcePath', resourcePath);
-  console.log('option', option);
-
-  console.log(express.static(resourcePath, option));
-
-  return express.static(resourcePath, option);
-}
-*/
 
 export { app };
